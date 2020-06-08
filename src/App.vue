e6b1516819ded62fec36ebc597feeda8146db579
Matt Priour <mpriour@esri.com>
2020-06-07 21:46:41 -0500

    Add header and qbutton

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <nav>
+      <router-link to="/" class="left branding">
+        <img src="./assets/mtg-colors.jpg" alt="The 5 color symbols of Magic the Gathering" />
+      </router-link>
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </nav>
+    <router-view class="main-view" />
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background-color: black;
+  color: #bababa;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
+  .main-view {
+    margin-top: 60px;
+  }
+  nav {
+    padding: 5px;
+    position: fixed;
+    background-color: yellow;
+    top: 0;
+    left: -10px;
+    width: 100vw;
+    .left {
+      float: left;
+    }
+    .branding {
+      img {
+        padding: 0 5px;
+        width: 2rem;
+        height: auto;
+      }
+    }
+    a {
+      font-weight: bold;
+      color: #2c3e50;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
 }
---
+++ src/components/QButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <router-link v-if="link" :to="link" tag="button"><slot></slot></router-link>
+    <button v-else><slot></slot></button>
+    <slot name="aside" class="aside"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    text: {
+      type: String,
+      default: ''
+    },
+    link: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.strikethrough button{
+  text-decoration: line-through red solid;
+}
+</style>
